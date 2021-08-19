ed7284e47b3b64345622f8229c56c71ee77d3580
Thariick <tharick12@hotmail.com>
2021-08-19 17:36:31 -0300

    Google API funcionando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState("AIzaSyBsdmVIL8uuiKFRfVhMmS3LsrjaybwKATY");
+
+
+
+
+
+
+  function handleChange(event) {
+    const book = event.target.value;
+    setBook(book);
+  }
+  function handleSubmit (event){
+    event.preventDefault(book);
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+book)
+    .then(data => {
+      setResult(data.data.items);
+    })
+  }
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "container">
+      <h1>Sitema para busca de livros</h1>
+      <form onSubmit = {handleSubmit}>
+        <div class = "form-group">
+          <input type = "text" onChange = {handleChange}
+          className = "form-control" 
+          placeholder = "Busque seu livro" 
+          autoComplete = "on"/>
+        </div>
+        <button type = "submit" className = "btn btn-danger">Buscar</button>
+      </form>
+ 
+        {result.map(book => (
+          <img src = {
+            book.volumeInfo.imageLinks === undefined
+              ? ""
+              : `${book.volumeInfo.imageLinks.thumbnail}`
+        } alt = {book.title}/>
+        ))}
+
     </div>
+  
   );
 }
 
